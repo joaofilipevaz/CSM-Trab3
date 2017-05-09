e00487bde8d4ef8be4ca2626775a98484db427a5
João Filipe Vaz <joaofilipevaz@gmail.com>
2017-05-09 20:40:36 +0100

    wip

--- trab3.py
+++ trab3.py
@@ -16,21 +16,17 @@ import Queue
 
 """
 Construa uma função (codiﬁcador) que para cada bloco de 8×8 da imagem original efectue a DCT bidimensional.
-Use por exemplo a instrução
-import cv2
-# DCT2D direta
-bloco_dct = cv2.dct(bloco)
 Veja a imagem com o conjunto dos blocos após a transformada.
 Construa uma função (descodiﬁcador) que faça a DCT inversa.
-#DCT2D inversa (IDCT2D)
-bloco_rec = cv2.dct(bloco_dct,[],cv2.DCT_INVERSE)
 Veriﬁque que a imagem é igual à original.
 """
 
+
 def codificador(bloco):
+    # DCT2D direta
     bloco_dct = cv2.dct(bloco)
 
 
-
 def descodificador(bloco_dct):
+    # DCT2D inversa (IDCT2D)
     bloco_rec = cv2.dct(bloco_dct, [], cv2.DCT_INVERSE)
